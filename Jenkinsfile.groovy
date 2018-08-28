71037cb45c1e521a567f03a599d634abaf36d11c
i324759 <abhishek.saxena01@sap.com>
2018-08-28 13:56:53 +0530

    fixed

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -17,29 +17,8 @@ pipeline {
         }
         stage ('Deploy to Staging'){
             steps {
-                build job: 'Deploy-to-staging'
+                build job: 'deploy-to-stage'
             }
         }
-
-        stage ('Deploy to Production'){
-            steps{
-                timeout(time:5, unit:'DAYS'){
-                    input message:'Approve PRODUCTION Deployment?'
-                }
-
-                build job: 'Deploy-to-Prod'
-            }
-            post {
-                success {
-                    echo 'Code deployed to Production.'
-                }
-
-                failure {
-                    echo ' Deployment failed.'
-                }
-            }
-        }
-
-
-    }
+      }
 }
